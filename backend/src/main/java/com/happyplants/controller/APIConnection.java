24448518efe85a996f38c6b0994bbdd3cc16bcd4
Amelie Music <amelielisaa@gmail.com>
2026-02-05 09:54:17 +0100

    FIRST ENDPOINT!
    We created the search-endpoint!
    
    Co-Authored-By: Carl Lundholm <carl_0221@hotmail.se>
    Co-Authored-By: Delaram Azad <157022246+delaramazad@users.noreply.github.com>

---
+++ backend/src/main/java/com/happyplants/controller/APIConnection.java
@@ -0,0 +1,41 @@
+package com.happyplants.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+@RestController
+@RequestMapping("/api")
+public class APIConnection {
+
+    @Value("${plant.api.token}")
+    private String plantApiKey;
+
+    @GetMapping("test")
+    public String test() {
+        return "Backend svarar svar: Koppling fungerar!";
+    }
+
+    @GetMapping("search")
+    public void search() throws IOException, InterruptedException {
+        String plantName = "orchid";
+        String url = String.format("https://perenual.com/api/v2/species-list?q=%s&page=1&hardiness=4-8&key=%s", plantName, plantApiKey);
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(url))
+                .GET()
+                .build();
+
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        System.out.println(response.body());
+        System.out.println(response.statusCode());
+        System.out.println(plantApiKey);
+    }
+}
