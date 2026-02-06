6ca0d341c77481267355d07a5ec07246beae61d0
Delaram Azad <aramazad@live.se>
2026-02-06 09:34:49 +0100

    html js fix för spinner
    Co-Authored-By: Amelie Music <amelielisaa@gmail.com>

--- backend/src/main/resources/static/js/script.js
+++ backend/src/main/resources/static/js/script.js
@@ -2,19 +2,31 @@ document.addEventListener('DOMContentLoaded', () => {
     const searchForm = document.getElementById('search-form');
     const searchInput = document.getElementById('plantID');
 
+    const spinner = document.getElementById('loading-spinner');
+
+
     searchForm.addEventListener('submit', async (event) => {
         event.preventDefault(); // Stoppa sidan från att laddas om
 
         const query = searchInput.value;
         if (!query) return;
 
+        if (spinner) {
+            spinner.style.display = 'block';
+            spinner.scrollIntoView({ behavior: 'smooth', block: 'center' });
+        }
+        //if (plantContainer) plantContainer.style.display = 'none';
+
+        //resetpanel();
+
         try {
-            // Vi anropar din backend-endpoint: /api/search?plantName=...
             const response = await fetch(`/api/search?plantName=${encodeURIComponent(query)}`);
             const data = await response.json();
 
             console.log("Data från API:", data);
             displayResults(data);
+
+
         } catch (error) {
             console.error("Kunde inte hämta data:", error);
             alert("could not reload")
