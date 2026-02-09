ceef9a36fd057a63a8193e1042752dd24bdb77ad
Hampus Svärd <187299286+HampusSvard@users.noreply.github.com>
2026-02-09 13:42:57 +0100

    unit tests for password requirements

---
+++ backend/Testing/PasswordTest.java
@@ -0,0 +1,69 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class PasswordTest {
+    private PasswordValidator passwordValidator;
+
+    @BeforeEach
+    public void setUp() {
+        passwordValidator = new PasswordValidator();
+    }
+
+    @Test
+    //All requirements are fulfilled
+    public void testValidPassword() {
+        boolean result = passwordValidator.isValid("AValidPassword123!");
+        assertTrue(result);
+    }
+
+    @Test
+    //All requirements are fulfilled with exact 12 characters
+    public void testValidPasswordBoundary() {
+        boolean result = passwordValidator.isValid("!12CharPass!");
+    }
+
+    @Test
+    //Missing 1 in length
+    public void testInvalidPassLengthBoundary() {
+        boolean result = passwordValidator.isValid("NotValid12!");
+        assertFalse(result);
+    }
+
+    @Test
+    //All requirements except for minimum 1 special char are fulfilled
+    public void testInvalidPassSpecialCharMissing() {
+        boolean result = passwordValidator.isValid("Abcdefg12345");
+        assertFalse(result);
+    }
+
+    @Test
+    //All requirements except for minimum 1 upper case letter are fulfilled
+    public void testInvalidPassUpperCaseMissing() {
+        boolean result = passwordValidator.isValid("abcdefg1234!");
+        assertFalse(result);
+    }
+
+    @Test
+    //All requirements except for minimum 1 lower case letter are fulfilled
+    public void testInvalidPassLowerCaseMissing() {
+        boolean result = passwordValidator.isValid("ABCDEFG1234!");
+        assertFalse(result);
+    }
+
+    @Test
+    //All requirements except for minimum 12 characters are fulfilled
+    public void testInvalidPassLengthMissing() {
+        boolean result = passwordValidator.isValid("Abc123!");
+        assertFalse(result);
+    }
+
+    @Test
+    //No requirement fulfilled
+    public void testInvalidPass() {
+        boolean result = passwordValidator.isValid("");
+        assertFalse(result);
+    }
+
+
+}
