8cf50233a231024f51f75686d86f3371ae2ceed6
Hampus Svärd <187299286+HampusSvard@users.noreply.github.com>
2026-02-12 14:25:07 +0100

    created Spring Data repos

---
+++ backend/src/main/java/com/happyplants/repository/UsersPlantRepository.java
@@ -0,0 +1,10 @@
+package com.happyplants.repository;
+
+import com.happyplants.model.UsersPlant;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+import java.util.UUID;
+
+public interface UsersPlantRepository extends JpaRepository<UsersPlant, UUID>, JpaSpecificationExecutor<UsersPlant> {
+}
