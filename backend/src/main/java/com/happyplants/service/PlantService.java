a39976a97821dede280f18b4c5c28b030a960745
Hampus Svärd <187299286+HampusSvard@users.noreply.github.com>
2026-02-16 21:28:06 +0100

    added plantservice and perenualid lookup support

---
+++ backend/src/main/java/com/happyplants/service/PlantService.java
@@ -0,0 +1,35 @@
+package com.happyplants.service;
+
+import com.happyplants.model.Plant;
+import com.happyplants.model.dto.PlantDTO;
+import com.happyplants.repository.PlantRepository;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class PlantService {
+    private final PlantRepository plantRepository;
+
+    public PlantService(PlantRepository plantRepository) {
+        this.plantRepository = plantRepository;
+    }
+
+    public Plant getOrCreatePlant(PlantDTO plantDTO) {
+        return plantRepository.findByPerenualId(plantDTO.getId())
+                .orElseGet(() -> plantRepository.save(convertDtoToPlant(plantDTO)))
+    }
+
+    private Plant convertDtoToPlant(PlantDTO plantDto) {
+        Plant plant = new Plant();
+        plant.setPerenualId(plantDto.getId());
+        plant.setCommonName(plantDto.getCommonName());
+        plant.setScientificName(plantDto.getScientificName() != null && plantDto.getScientificName().length > 0 ? plantDto.getScientificName()[0] : "Unknown");
+        plant.setFamilyName(plantDto.getFamily());
+        plant.setGenus(plantDto.getGenus());
+        plant.setWateringDescription(plantDto.getWatering());
+        plant.setSunDescription(plantDto.getSunlight());
+        return plant;
+    }
+
+
+}
