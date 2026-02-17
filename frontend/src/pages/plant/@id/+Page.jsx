e5c62d2f45ec4d9a151c22066b5d2ab23494aef7
ThorANilsson <thor.a.nilsson@gmail.com>
2026-02-17 18:41:52 +0100

    Added Vike + other
    commit ec53fdedcb067327348a9693b1c6f968ff1126bf
    Author: ThorANilsson <thor.a.nilsson@gmail.com>
    Date:   Tue Feb 17 18:40:01 2026 +0100
    
        Moved existing components around for clearer page separation, using Vike
    
    commit b49844ac7252ec859f6e029e242588b2fad6e233
    Author: ThorANilsson <thor.a.nilsson@gmail.com>
    Date:   Tue Feb 17 18:39:29 2026 +0100
    
        Moved CSS files to /css directory
    
    commit 9c901755048b586af1a4f1378ea07f3608a861d4
    Author: ThorANilsson <thor.a.nilsson@gmail.com>
    Date:   Tue Feb 17 18:39:04 2026 +0100
    
        Added Vike
    
        - Allows server side rendering, which can make protected routes easier to maintain
        - Much cleaner, file based router

---
+++ frontend/src/pages/plant/@id/+Page.jsx
@@ -0,0 +1,37 @@
+import { useEffect, useState } from "react";
+import { usePageContext } from "vike-react/usePageContext";
+import { API_BASE_URL } from "@/config";
+
+import "@/css/index.css";
+import "@/css/style.css";
+import "@/css/App.css";
+
+export default function Page() {
+  const pageContext = usePageContext();
+  const { id } = pageContext.routeParams;
+
+  const [plant, setPlant] = useState(null);
+
+  useEffect(() => {
+    if (!id) return;
+
+    fetch(`${API_BASE_URL}/plants/${id}`)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log("Received plant detail:", data);
+        console.log("Plant imageUrl:", data.imageUrl);
+        setPlant(data);
+      })
+      .catch((err) => console.error("Error fetching plant details:", err));
+  }, [id]);
+
+  if (!plant) return <p>Loading...</p>;
+
+  return (
+    <div>
+      <h1>{plant.name}</h1>
+      <img src={plant.imageUrl} alt={plant.name} />
+      <p>{plant.description}</p>
+    </div>
+  );
+}
