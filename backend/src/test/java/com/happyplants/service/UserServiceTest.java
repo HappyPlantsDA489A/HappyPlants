8674d05eff147b6eeb6134860e8fef04ad6332b3
Amelie Music <amelielisaa@gmail.com>
2026-03-15 13:42:09 +0100

    Added DisplayName annotations to the tests

--- backend/src/test/java/com/happyplants/service/UserServiceTest.java
+++ backend/src/test/java/com/happyplants/service/UserServiceTest.java
@@ -9,6 +9,7 @@ import com.happyplants.repository.UserRepository;
 import com.happyplants.util.PasswordValidator;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -52,87 +53,102 @@ public class UserServiceTest {
         user.setPasswordHash("oldHashedPassword");
     }
 
-    @Test
-    void shouldReturnCurrentUser() {
-        UUID id = UUID.randomUUID();
-        User user = new User();
-        user.setId(id);
+    @Nested
+    class findUserByIdTests {
 
-        when(auth.getName()).thenReturn(id.toString());
-        when(userRepository.findById(id)).thenReturn(Optional.of(user));
+        @Test
+        void shouldReturnCurrentUser() {
+            UUID id = UUID.randomUUID();
+            User user = new User();
+            user.setId(id);
 
-        User result = userService.getCurrentUser(auth);
+            when(auth.getName()).thenReturn(id.toString());
+            when(userRepository.findById(id)).thenReturn(Optional.of(user));
 
-        assertNotNull(result);
-        assertEquals(id, result.getId());
-        verify(userRepository).findById(id);
+            User result = userService.getCurrentUser(auth);
+
+            assertNotNull(result);
+            assertEquals(id, result.getId());
+            verify(userRepository).findById(id);
+        }
+
+        @Test
+        void shouldThrowExceptionUserNotFound() {
+            UUID id = UUID.randomUUID();
+
+            when(auth.getName()).thenReturn(id.toString());
+            when(userRepository.findById(id)).thenReturn(Optional.empty());
+
+            assertThrows(UserNotFoundException.class,
+                    () -> userService.getCurrentUser(auth));
+
+            verify(userRepository).findById(id);
+        }
     }
 
-    @Test
-    void shouldThrowExceptionUserNotFound() {
-        UUID id = UUID.randomUUID();
+    @Nested
+    class ChangePasswordTests {
 
-        when(auth.getName()).thenReturn(id.toString());
-        when(userRepository.findById(id)).thenReturn(Optional.empty());
+        @Test
+        @DisplayName("TC-USER-03.1: Ensures that changePassword() updates the password and saves the user with the new password.")
+        void shouldUpdatePasswordAndSaveUser_changePassword() {
+            ChangePasswordRequest passwordRequest = new ChangePasswordRequest("CurrentPassword123!", "NewPassword123!");
 
-        assertThrows(UserNotFoundException.class,
-                () -> userService.getCurrentUser(auth));
+            lenient().when(passwordEncoder.matches("CurrentPassword123!", "oldHashedPassword")).thenReturn(true);
+            lenient().when(passwordValidator.validate("NewPassword123!")).thenReturn(List.of());
+            lenient().when(passwordEncoder.encode("NewPassword123!")).thenReturn("newHashedPassword");
 
-        verify(userRepository).findById(id);
+            userService.changePassword(user, passwordRequest);
+
+            assertEquals("newHashedPassword", user.getPasswordHash());
+
+            verify(passwordEncoder).matches("CurrentPassword123!", "oldHashedPassword");
+            verify(passwordEncoder).encode("NewPassword123!");
+            verify(userRepository).save(user);
+        }
+
+        @Test
+        @DisplayName("TC-USER-03.1: Verifies that InvalidLoginCredentialsException is thrown if the user enters the wrong current password while attempting to change their password.")
+        void shouldThrowInvalidLoginCredentialsException_changePassword() {
+            ChangePasswordRequest passwordRequest = new ChangePasswordRequest("WrongPassword", "NewPassword");
+            when(passwordEncoder.matches("WrongPassword", "oldHashedPassword")).thenReturn(false);
+
+            assertThrows(InvalidLoginCredentialsException.class, () -> userService.changePassword(user, passwordRequest));
+
+            verify(userRepository, never()).save(any());
+            verify(passwordValidator, never()).validate(anyString());
+            verify(passwordEncoder, never()).encode(anyString());
+        }
+
+        @Test
+        @DisplayName("TC-USER-03.1: Verifies that WeakPasswordException is thrown if the user enters a weak new password.")
+        void shouldThrowWeakPasswordException_changePassword() {
+            ChangePasswordRequest passwordRequest = new ChangePasswordRequest("WrongPassword", "NewPassword");
+
+            lenient().when(passwordEncoder.matches("WrongPassword", "oldHashedPassword")).thenReturn(true);
+            lenient().when(passwordValidator.validate("NewPassword")).thenReturn(List.of());
+            lenient().when(passwordEncoder.encode("NewPassword")).thenReturn("newHashedPassword");
+
+            assertThrows(WeakPasswordException.class, () -> userService.changePassword(user, passwordRequest));
+
+            verify(userRepository, never()).save(any());
+            verify(passwordValidator, never()).validate(anyString());
+            verify(passwordEncoder, never()).encode(anyString());
+        }
     }
 
-    @Test
-    void shouldUpdatePasswordAndSaveUser_changePassword() {
-        ChangePasswordRequest passwordRequest = new ChangePasswordRequest("CurrentPassword123!", "NewPassword123!");
+    @Nested
+    class DeleteUserTests {
+        
+        @Test
+        @DisplayName("TC-USER-04: Verifies that a user account can be deleted.")
+        void shouldDeleteUser() {
+            User userToDelete = new User();
+            userToDelete.setId(UUID.randomUUID());
 
-        lenient().when(passwordEncoder.matches("CurrentPassword123!", "oldHashedPassword")).thenReturn(true);
-        lenient().when(passwordValidator.validate("NewPassword123!")).thenReturn(List.of());
-        lenient().when(passwordEncoder.encode("NewPassword123!")).thenReturn("newHashedPassword");
+            userService.deleteUser(userToDelete);
 
-        userService.changePassword(user, passwordRequest);
-
-        assertEquals("newHashedPassword", user.getPasswordHash());
-
-        verify(passwordEncoder).matches("CurrentPassword123!", "oldHashedPassword");
-        verify(passwordEncoder).encode("NewPassword123!");
-        verify(userRepository).save(user);
-    }
-
-    @Test
-    void shouldThrowInvalidLoginCredentialsException_changePassword() {
-        ChangePasswordRequest passwordRequest = new ChangePasswordRequest("WrongPassword", "NewPassword");
-        when(passwordEncoder.matches("WrongPassword", "oldHashedPassword")).thenReturn(false);
-
-        assertThrows(InvalidLoginCredentialsException.class, () -> userService.changePassword(user, passwordRequest));
-
-        verify(userRepository, never()).save(any());
-        verify(passwordValidator, never()).validate(anyString());
-        verify(passwordEncoder, never()).encode(anyString());
-    }
-
-    @Test
-    void shouldThrowWeakPasswordException_changePassword() {
-        ChangePasswordRequest passwordRequest = new ChangePasswordRequest("WrongPassword", "NewPassword");
-
-        lenient().when(passwordEncoder.matches("WrongPassword", "oldHashedPassword")).thenReturn(true);
-        lenient().when(passwordValidator.validate("NewPassword")).thenReturn(List.of());
-        lenient().when(passwordEncoder.encode("NewPassword")).thenReturn("newHashedPassword");
-
-        assertThrows(WeakPasswordException.class, () -> userService.changePassword(user, passwordRequest));
-
-        verify(userRepository, never()).save(any());
-        verify(passwordValidator, never()).validate(anyString());
-        verify(passwordEncoder, never()).encode(anyString());
-    }
-
-    @Test
-    @DisplayName("should delete user")
-    void shouldDeleteUser() {
-        User userToDelete = new User();
-        userToDelete.setId(UUID.randomUUID());
-
-        userService.deleteUser(userToDelete);
-
-        verify(userRepository).delete(userToDelete);
+            verify(userRepository).delete(userToDelete);
+        }
     }
 }
